609bad18485f7ddfbe526237b2ca05a6ae0b57e3
Hugo <hugo.dlhp@gmail.com>
2024-02-06 11:07:38 +0100

    commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,39 @@
 const juego = document.getElementById("juego")
+const marcador = document.getElementById("puntos")
 
 
 const ctx = juego.getContext("2d")
 
+const DIRECCIONES = {
+    ARRIBA: 1,
+    ABAJO: 2,
+    IZQUIERDA: 3,
+    DERECHA: 4
+}
 
-const mapWidth = 500
+const FPS = 1000 / 10
 
-const mapHeight = 450
+const mapWidth = 370
+
+const mapHeight = 280
+
+let direccion = DIRECCIONES.DERECHA
+let cabezaPosX = 10, cabezaPosY = 10
+
+let coso = [
+    { posX: 10, posY: 10 }
+]
+
+let posicionesDeCoso = new Set()
+
+let comida = crearComida()
+
+let puntos = 0
+
+let objetivo_puntos = 0;
+
+let intervaloDeJuego;
+
+const aumentarMarcador = () => {
+    marcador.innerText = puntos;
+}
