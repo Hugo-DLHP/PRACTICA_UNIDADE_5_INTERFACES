cd8244c2143baeec046cf2f7d927e67a58a7e88f
Hugo <hugo.dlhp@gmail.com>
2024-02-09 14:25:24 +0100

    commit nuevo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const juego = document.getElementById("juego")
-const marcador = document.getElementById("puntos")
+const marcador = document.getElementById("marcador")
+const start = document.getElementById("start")
 
 
 const ctx = juego.getContext("2d")
@@ -13,9 +14,9 @@ const DIRECCIONES = {
 
 const FPS = 1000 / 10
 
-const mapWidth = 370
+const mapWidth = 500
 
-const mapHeight = 280
+const mapHeight = 450
 
 let direccion = DIRECCIONES.DERECHA
 let cabezaPosX = 10, cabezaPosY = 10
@@ -37,3 +38,40 @@ let intervaloDeJuego;
 const aumentarMarcador = () => {
     marcador.innerText = puntos;
 }
+
+function dibujarComida() {
+    ctx.beginPath()
+    ctx.rect(comida.posX, comida.posY, 10, 10)
+    ctx.stroke()
+}
+
+function crearComida() {
+    let min = Math.ceil(5);
+    let max = Math.floor(25);
+    let posX = (Math.floor(Math.random() * (max - min)) + min) * 10
+    let posY = (Math.floor(Math.random() * (max - min)) + min) * 10
+
+    if (posicionesDeCoso.has(`${posX}${posY}`)) {
+        return crearComida()
+    }
+
+    return { posX, posY }
+}
+
+function setGame() {
+    puntos = 0;
+    coso.length = 1;
+    marcador.innerText = puntos;
+    cabezaPosX = 10;
+    cabezaPosY = 10;
+    direccion = DIRECCIONES.DERECHA
+}
+
+const startGame = () => {
+    setGame()
+    gameLoop()
+    document.addEventListener('keyup', direccionEventos);
+    intervaloDeJuego = setInterval(gameLoop, FPS);
+}
+
+start.addEventListener('click', startGame)
